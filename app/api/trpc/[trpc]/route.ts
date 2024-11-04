7ea1e55027c530b56cfb52f9fe9747637e8e62c4
Hannah Willoughby <hannah@hannahwilloughby.dev>
2024-11-04 13:45:36 -0500

    Fix database connection

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,13 +1,15 @@
 import { appRouter } from "@/server"
+import { auth } from "@clerk/nextjs/server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch"
 
 function handler(req: Request) {
+  const { userId } = auth();
   return fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
     createContext: () => ({
-      userId: null,
+      userId,
       req: req as any // Type assertion to NextApiRequest
     })
   })
