f46328e5def4ccc21d7fbf100fc889731b858650
Hannah Willoughby <hannah@tcah.com>
2025-01-25 08:03:04 -0500

    Add theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import './globals.css';
-import { ThemeProvider } from '@/components/theme-provider';
+import { ThemeProvider } from '@/components/providers/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
 import { trpc } from '@/lib/trpc';
 import { ClerkProvider } from '@clerk/nextjs';
